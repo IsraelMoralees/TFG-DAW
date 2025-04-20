000d94da65c57a73e4b20be7361e42a4fca19dbd
IsraelMoralees <israelmoralessanchez17@gmail.com>
2025-04-20 13:12:37 +0200

    1º commit

---
+++ easykey/src/main/java/com/easykey/easykey/EasykeyApplication.java
@@ -0,0 +1,13 @@
+package com.easykey.easykey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasykeyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasykeyApplication.class, args);
+	}
+
+}
